3d5121b468043f66af35af56b5c6e32331ebf135
rxhul18 <rahulshah0728@gmail.com>
2025-01-09 19:47:24 +0530

    Few ui change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, data } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Product from "./components/Product";
@@ -7,40 +7,52 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Auth } from "@supabase/auth-ui-react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient('https://vzxikxhahtpqyancakzw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6eGlreGhhaHRwcXlhbmNha3p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0MTIwNzgsImV4cCI6MjA1MTk4ODA3OH0._FrFrHsfQUneV3vt-0sbAHz6EYRa_DgVHzUtLINrzFo')
-// Main App component
+const supabase = createClient(
+  "https://vzxikxhahtpqyancakzw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6eGlreGhhaHRwcXlhbmNha3p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0MTIwNzgsImV4cCI6MjA1MTk4ODA3OH0._FrFrHsfQUneV3vt-0sbAHz6EYRa_DgVHzUtLINrzFo"
+);
+
 const App = () => {
   const [session, setSession] = useState(null);
-  const [mailId, serMailId] = useState(null);
+  const [mailId, setMailId] = useState(null);
+
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
+      setMailId(session?.user?.email);
     });
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
+      setMailId(session?.user?.email);
     });
-    serMailId(session?.user?.email)
+
     return () => subscription.unsubscribe();
   }, []);
 
   if (!session) {
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
-  } else {
     return (
-      <Router>
-        <div className="min-w-full min-h-full">
-          <Navbar mail={mailId}/>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Product mail={mailId}/>} />
-          </Routes>
+      <div className="w-full">
+        <div className="container max-w-3xl">
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
         </div>
-      </Router>
+      </div>
     );
   }
+
+  return (
+    <Router>
+      <div className="min-w-full min-h-full">
+        <Navbar mail={mailId} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Product mail={mailId} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@
 const Product = ({mail}) => {
   return (
       <div className="p-4 w-full h-full flex justify-center">
-        <h1 className="text-3xl font-bold mb-4">Welcome, {mail}</h1>
+        <h1 className="text-3xl font-bold mb-4">Welcome to Product page, {mail}</h1>
       </div>
   )
 }
